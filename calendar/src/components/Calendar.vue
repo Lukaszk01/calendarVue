a9b41da9a848393b985f1cc6157c081ddd03940f
Lukasz <lukaszkkoziel@gmail.com>
2020-10-28 12:54:33 +0900

    rebuilded calendar

---
+++ calendar/src/components/Calendar.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="calendar">
+    <div @click='display' class="btn">1</div>
+    <div @click='display' class="btn">2</div>
+    <div @click='display' class="btn">3</div>
+    <div @click='display' class="btn">4</div>
+    <div @click='display' class="btn">5</div>
+    <div @click='display' class="btn">6</div>
+    <div @click='display' class="btn">7</div>
+    <div @click='display' class="btn">8</div>
+    <div @click='display' class="btn">9</div>
+    <div @click='display' class="btn">10</div>
+    <div @click='display' class="btn">11</div>
+    <div @click='display' class="btn">12</div>
+    <div @click='display' class="btn">12</div>
+    <div @click='display' class="btn">13</div>
+    <div @click='display' class="btn">14</div>
+    <div @click='display' class="btn">15</div>
+    <div @click='display' class="btn">16</div>
+    <div @click='display' class="btn">17</div>
+    <div @click='display' class="btn">18</div>
+    <div @click='display' class="btn">19</div>
+    <div @click='display' class="btn">20</div>
+    <div @click='display' class="btn">21</div>
+    <div @click='display' class="btn">23</div>
+    <div @click='display' class="btn">24</div>
+    <div @click='display' class="btn">25</div>
+    <div @click='display' class="btn">26</div>
+    <div @click='display' class="btn">27</div>
+    <div @click='display' class="btn">28</div>
+    <div @click='display' class="btn">29</div>
+    <div @click='display' class="btn">30</div>
+    <div @click='display' class="btn">31</div>
+
+  </div>
+</template>
+
+
+<style scoped>
+.calculator {
+  margin: 0 auto;
+  width: 400px;
+  font-size: 40px;
+  grid-auto-rows: minmax(50px, auto);
+}
+.btn {
+  background-color: #F2F2F2;
+  border: 1px solid #999;
+  width: 150px;
+  height: 200px;
+}
+
+</style>
