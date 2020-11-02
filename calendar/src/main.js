e0e79a35c7bf95150f36325138c0520757426424
Lukasz <lukaszkkoziel@gmail.com>
2020-11-02 16:31:16 +0900

    conflict

--- calendar/src/main.js
+++ calendar/src/main.js
@@ -1,6 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+import movable from "v-movable";
+Vue.use(movable);
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 Vue.config.productionTip = false
 
 new Vue({
