e2f7ff325070f9308d9c5f2c8060f61633fa738e
Lukasz <lukaszkkoziel@gmail.com>
2020-11-02 12:22:21 +0900

    cards

--- calendar/src/components/Calendar.vue
+++ calendar/src/components/Calendar.vue
@@ -3,43 +3,60 @@
 <template>
   <div class="container">
     <div class="calendar">
-      <movable @click="append('1')" class="btn">{{ current || '1'}}</movable >
-      <movable @click='display' id="btn2" class="btn">2</movable>
-      <movable @click='onClick' id="btn" :class="{ red : deleteClicked }">3</movable>
-      <movable @click='display' class="btn">4</movable>
+      <div @click="append('1')" class="btn">{{ current || '1'}}</div >
+      <div @click='display' id="btn2" class="btn">2</div>
+      <div @click='onClick' id="btn" :class="{ red : deleteClicked }">3</div>
+      <div @click='display' class="btn">4</div>
 
-    <movable @click='display' class="btn">5</movable>
-    <movable @click='display' class="btn">6</movable>
-    <movable @click='display' class="btn">7</movable>
-    <movable @click='display' class="btn">8</movable>
-    <movable @click='display' class="btn">9</movable>
-    <movable @click='display' class="btn">10</movable>
-    <movable @click='display' class="btn">11</movable>
-    <movable @click='display' class="btn">12</movable>
-    <movable @click='display' class="btn">12</movable>
-    <movable @click='display' class="btn">13</movable>
-    <movable @click='display' class="btn">14</movable>
-    <movable @click='display' class="btn">15</movable>
-    <movable @click='display' class="btn">16</movable>
-    <movable @click='display' class="btn">17</movable>
-    <movable @click='display' class="btn">18</movable>
-    <movable @click='display' class="btn">19</movable>
-    <movable @click='display' class="btn">20</movable>
-    <movable @click='display' class="btn">21</movable>
-    <movable @click='display' class="btn">23</movable>
-    <movable @click='display' class="btn">24</movable>
-    <movable @click='display' class="btn">25</movable>
-    <movable @click='display' class="btn">26</movable>
-    <movable @click='display' class="btn">27</movable>
-    <movable @click='display' class="btn">28</movable>
-    <movable @click='display' class="btn">29</movable>
-    <movable @click='display' class="btn">30</movable>
-    <movable @click='display' class="btn">
-      <span>Your task:</span>
-        <p style="white-space: pre-line;">{{ message }}</p>
-        <br>
-        <textarea v-model="message" placeholder="Type"></textarea>
-        31</movable>
+    <div @click='display' class="btn">5</div>
+    <div @click='display' class="btn">6</div>
+    <div @click='display' class="btn">7</div>
+    <div @click='display' class="btn">8</div>
+    <div @click='display' class="btn">9</div>
+    <div @click='display' class="btn">10</div>
+    <div @click='display' class="btn">11</div>
+    <div @click='display' class="btn">12</div>
+    <div @click='display' class="btn">12</div>
+    <div @click='display' class="btn">13</div>
+    <div @click='display' class="btn">14</div>
+    <div @click='display' class="btn">15</div>
+    <div @click='display' class="btn">16</div>
+    <div @click='display' class="btn">17</div>
+    <div @click='display' class="btn">18</div>
+    <div @click='display' class="btn">19</div>
+    <div @click='display' class="btn">20</div>
+    <div @click='display' class="btn">21</div>
+    <div @click='display' class="btn">23</div>
+    <div @click='display' class="btn">24</div>
+    <div @click='display' class="btn">25</div>
+    <div @click='display' class="btn">26</div>
+    <div @click='display' class="btn">27</div>
+    <div @click='display' class="btn">28</div>
+    <div @click='display' class="btn">
+      <b-list-group>
+      <b-list-group-item>
+        <div>
+          <b-form-textarea
+            id="textarea"
+            v-model="text"
+            placeholder="Enter something..."
+            rows="3"
+            max-rows="6"></b-form-textarea>
+          <pre class="mt-3 mb-0">{{ text }}</pre>
+        </div>
+      </b-list-group-item>
+        <b-list-group-item>2.</b-list-group-item>
+        <b-list-group-item>3.</b-list-group-item>
+        <b-list-group-item>4.</b-list-group-item>
+        <b-list-group-item>5.</b-list-group-item>
+    </b-list-group>29</div>
+
+
+    <div @click='display' class="btn">
+    30</div>
+
+    <div @click='display' class="btn">
+        31</div>
   </div>
   </div>
 <!--   <div style="position:relative; margin:50px;">
@@ -54,6 +71,7 @@
   data() {
     return {
       previous: null,
+      text: '',
       current: '',
       operator: null,
       operatorClicked: false,
@@ -117,9 +135,16 @@
         color: white;
       }
 textarea {
-  width: 120px;
+  width: 100px;
+  height: 10px;
   background-color: #F2F2F2;
   border: none;
+  font-size: 5px;
+}
+
+.list-group-item {
+  width: 130px;
+  height: 35px;
 }
 
 
